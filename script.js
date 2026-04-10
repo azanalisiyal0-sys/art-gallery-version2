c44cc762f12477fa08f3e9bb1e1275fe0d32b50d
Azan Ali <azanalisiyal0@gmail.com>
2026-04-10 20:05:59 +0500

    Removed unnecessary code

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-// //Animations
-// const section = document.querySelectorAll(".fade");
-// const revealOnScroll = () => {
-//     const windowHeight = window.innerHeight;
-//     section.forEach(section => {
-//         const sectionTop = section.getBoundingClientRect().top;
-
-//         if(sectionTop < windowHeight - 100) {
-//             section.classList.add("show");
-//         }
-//     });
-// };
-
-// window.addEventListener("scroll", revealOnScroll);
-// revealOnScroll();
 
 //Mobile Navigation Toggle
 
